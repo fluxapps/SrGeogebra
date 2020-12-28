7b8811b10521a34ba2d2e79e04afa5f27c896b19
jdolf <jd@studer-raimann.ch>
2020-12-28 16:10:13 +0100

    Directory migration

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . "/vendor/srag/dic/src/PHPVersionChecker.php";
 
 $id = "srgg";
-$version = "1.0.0";
+$version = "1.0.1";
 $ilias_min_version = "5.3.0";
 $ilias_max_version = "6.9.999";
 $responsible = "studer + raimann ag - Team Custom 1";
--- sql/dbupdate.php
+++ sql/dbupdate.php
@@ -23,3 +23,46 @@ use srag\Plugins\SrGeogebra\Config\ConfigAdvancedGeogebraFormGUI;
 
 Repository::getInstance()->setValue(ConfigAdvancedGeogebraFormGUI::KEY_IMMUTABLE, []);
 ?>
+<#5>
+<?php
+use srag\Plugins\SrGeogebra\Upload\UploadService;
+global $DIC;
+$ilDB = $DIC->database();
+
+$result = $ilDB->query("SELECT page_id, parent_id, content FROM page_object");
+
+$resultAssoc = $ilDB->fetchAll($result);
+
+foreach ($resultAssoc as $entry) {
+    $xml = new SimpleXMLElement($entry["content"]);
+
+    if (!isset($xml->PageContent->Plugged)) {
+        continue;
+    }
+
+    $plugin_name = (string) $xml->PageContent->Plugged->attributes()->PluginName;
+    $file_name = (string) $xml->PageContent->Plugged->xpath("PluggedProperty[@Name='fileName']")[0];
+
+    if (!is_null($plugin_name) && !empty($plugin_name) && $plugin_name === "SrGeogebra") {
+        $page_id = $entry["page_id"];
+        $parent_id = $entry["parent_id"];
+
+        $old_file_location = sprintf("./data/default/geogebra/%s", $file_name);
+
+        if (file_exists($old_file_location) && !empty($page_id) && !empty($parent_id)) {
+            $new_file_location = UploadService::evaluateFileName($file_name, $page_id, $parent_id);
+
+            //rename($old_file_location, $new_file_location);
+
+            $prop_file_name = $xml->PageContent->Plugged->xpath("PluggedProperty[@Name='fileName']");
+            $prop_legacy_file_name = $xml->PageContent->Plugged->xpath("PluggedProperty[@Name='legacyFileName']");
+            $prop_file_name[0][0] = $new_file_location;
+            $prop_legacy_file_name[0][0] = $new_file_location;
+
+            $update_query = sprintf("UPDATE page_object SET content = %s WHERE page_id = %s AND parent_id = %s", $ilDB->quote(str_replace("<?xml version=\"1.0\"?>\n", '', $xml->asXML()), 'text'), $page_id, $parent_id);
+
+            $ilDB->query($update_query);
+        }
+    }
+}
+?>

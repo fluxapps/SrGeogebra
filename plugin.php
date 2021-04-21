177f9daf14cb2b8abbfc2700e568997da2a16e1e
jdolf <jd@studer-raimann.ch>
2021-04-21 12:15:59 +0200

    Update Version

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . "/vendor/srag/dic/src/PHPVersionChecker.php";
 
 $id = "srgg";
-$version = "1.1.0";
+$version = "1.1.1";
 $ilias_min_version = "5.3.0";
 $ilias_max_version = "6.9";
 $responsible = "studer + raimann ag - Team Custom 1";

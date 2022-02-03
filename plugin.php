f5cbba12287a30e715a56385f00c7a4534283d60
fluxms <martin@fluxlabs.ch>
2022-02-03 02:49:26 +0100

    ILIAS 7

--- plugin.php
+++ plugin.php
@@ -3,8 +3,8 @@
 require_once __DIR__ . "/vendor/srag/dic/src/PHPVersionChecker.php";
 
 $id = "srgg";
-$version = "1.0.0";
-$ilias_min_version = "5.3.0";
-$ilias_max_version = "6.9.999";
-$responsible = "studer + raimann ag - Team Custom 1";
-$responsible_mail = "support-custom1@studer-raimann.ch";
+$version = "1.1.0";
+$ilias_min_version = "6.0";
+$ilias_max_version = "7.999";
+$responsible = "fluxlabs ag";
+$responsible_mail = "support@fluxlabs.ch";
